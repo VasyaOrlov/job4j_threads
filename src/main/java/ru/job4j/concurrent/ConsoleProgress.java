0d3d48b8f8f93a37d2bec87bea61eb7704d6a829
VasiliiOrlov <ov_prog@mail.ru>
2022-08-10 00:20:42 +0300

    Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        char[] process = {'-', '\\', '|', '/'};
+        while (!Thread.currentThread().isInterrupted()) {
+            for (char c : process) {
+                System.out.print("\rLoading..." + c);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+}

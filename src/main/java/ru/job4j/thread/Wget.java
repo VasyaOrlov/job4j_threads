2976ae65457e3b7351f6afb9479b10e7e13be225
VasiliiOrlov <ov_prog@mail.ru>
2022-08-20 07:44:04 +0300

    Скачивание файла с ограничением. [#144271] fix1

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -6,33 +6,44 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Wget implements Runnable {
+
+    private final static int SECOND = 1000;
     private final String url;
     private final int speed;
+    private final String name;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String name) {
         this.url = url;
         this.speed = speed;
+        this.name = name;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(name)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             long before = System.currentTimeMillis();
+            int downloadData = 0;
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long after = System.currentTimeMillis();
-                long time = after - before;
-                if (time < speed) {
-                    try {
-                        Thread.sleep(speed - time);
-                        before = after;
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
+                downloadData += dataBuffer.length;
+                if (downloadData >= speed) {
+                    long after = System.currentTimeMillis();
+                    long time = after - before;
+                    System.out.println(time);
+                    if (time < SECOND) {
+                        try {
+                            Thread.sleep(SECOND - time);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
+                    before = System.currentTimeMillis();
+                    downloadData = 0;
                 }
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -40,7 +51,7 @@ public class Wget implements Runnable {
     }
 
     private static void validate(String[] args) {
-        if (args.length < 2) {
+        if (args.length < 3) {
             throw new IllegalArgumentException("Отсутствуют параметры запуска");
         }
         try {
@@ -51,13 +62,17 @@ public class Wget implements Runnable {
         if (Integer.parseInt(args[1]) <= 0) {
             throw new IllegalArgumentException("Задайте значение скорости больше 0");
         }
+        if (args[2].length() < 3 || !args[2].contains(".")) {
+            throw new IllegalArgumentException("Недопустимое имя файла");
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
         validate(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String name = args[2];
+        Thread wget = new Thread(new Wget(url, speed, name));
         wget.start();
         wget.join();
     }

908cc6bcfa3a0cb54a7696b4ea64621d5d93ee7f
VasiliiOrlov <ov_prog@mail.ru>
2022-09-13 15:22:58 +0300

    Junit тест для блокирующей очереди. [#68589]

--- src/main/java/ru/job4j/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -37,4 +37,8 @@ public class SimpleBlockingQueue<T> {
         }
         return queue.poll();
     }
+
+    public synchronized boolean isEmpty() {
+        return !queue.isEmpty();
+    }
 }
--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -2,6 +2,12 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.*;
 
 class SimpleBlockingQueueTest {
@@ -30,5 +36,65 @@ class SimpleBlockingQueueTest {
         first.join();
         second.join();
         assertThat(queue.poll()).isEqualTo(2);
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 3).forEach(
+                        queue::offer
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isEqualTo(Arrays.asList(0, 1, 2));
+    }
+
+    @Test
+    public void OfferAndPollTest() throws InterruptedException {
+        List<Integer> list = new ArrayList<>();
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < 5; i++) {
+                        queue.offer(i);
+                        System.out.println(i);
+                    }
+                }
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    while (queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            list.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(list).isEqualTo(Arrays.asList(0, 1, 2, 3, 4));
     }
 }

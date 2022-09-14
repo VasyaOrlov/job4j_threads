08a816ef82c6111971e5510693a096e376b41a11
VasiliiOrlov <ov_prog@mail.ru>
2022-09-14 14:16:18 +0300

    ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/mail/EmailNotification.java
@@ -0,0 +1,32 @@
+package ru.job4j.mail;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(() -> {
+            String subject = "Notification " + user.userName() + " to email " + user.email();
+            String body = "Add a new event to " + user.userName();
+            send(subject, body, user.email());
+        });
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+}
